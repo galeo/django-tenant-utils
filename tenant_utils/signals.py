6de0f28674c9e7f0eaa197651d4b5de442977ae0
Moogen Tian <moogentian@outlook.com>
2018-11-19 17:27:27 +0800

    feat(tenants): connect or disconnect from public user to tenant user

--- tenant_utils/signals.py
+++ tenant_utils/signals.py
@@ -7,6 +7,12 @@ tenant_user_removed = Signal(providing_args=["user", "tenant"])
 # An existing user added to a tenant
 tenant_user_added = Signal(providing_args=["user", "tenant"])
 
+# An existing user is connected to a tenant user
+tenant_user_connected = Signal(providing_args=["user", "tenant", "tenant_user"])
+
+# An existing user is disconnected from a tenant user
+tenant_user_disconnected = Signal(providing_args=["user", "tenant", "tenant_user"])
+
 # A new user is created
 tenant_user_created = Signal(providing_args=["user"])
 
--- tenant_utils/tenants.py
+++ tenant_utils/tenants.py
@@ -11,7 +11,9 @@ from django_tenants.utils import get_public_schema_name, get_tenant_model
 from . import get_tenant_user_model
 from .signals import (
     tenant_user_added,
-    tenant_user_removed
+    tenant_user_removed,
+    tenant_user_connected,
+    tenant_user_disconnected
 )
 from .utils import schema_required, get_permissions_model
 from .exceptions import InactiveError, ExistsError, DeleteError, SchemaError
@@ -49,6 +51,111 @@ class TenantBase(TenantMixin):
         else:
             raise DeleteError("Not supported -- delete_tenant() should be used.")
 
+    def _link_to_tenant_user(self, user_obj, tenant_user):
+        """
+        Link the public user to the tenant user.
+        """
+        fields = self.__class__.users.field.remote_field.through_fields + (
+            'organization_user',)
+        self.__class__.users.through._default_manager.create(
+            **dict(zip(fields, (self, user_obj, tenant_user.pk))))
+
+    def _unlink_from_tenant(self, user_obj):
+        """
+        Unlink the public user from the tenant.
+        """
+        fields = self.__class__.users.field.remote_field.through_fields
+        self.__class__.users.through._default_manager.filter(
+            **dict(zip(fields, (self, user_obj)))).delete()
+
+    def _check_user_exists(self, user_obj):
+        """
+        Check if the public user already linked here.
+        """
+        # User already connected to a tenant user
+        if self.users.filter(id=user_obj.id).exists():
+            raise ExistsError("User already added to tenant: %s" % user_obj)
+
+        # User already linked to a user inside the tenant, due to dirty data
+        linked_tenant_user = get_tenant_user_model().objects.filter(
+            supervisor_id=user_obj.id).first()
+        if linked_tenant_user:
+            raise ExistsError(
+                "User already linked to a user inside the tenant: {}".format(
+                    linked_tenant_user))
+
+    @schema_required
+    @transaction.atomic
+    def connect_user(self, user_obj, tenant_user):
+        """
+        Connect a public user to a tenant user.
+        """
+        if self.schema_name == get_public_schema_name():
+            raise SchemaError(
+                "It's not allowed to connect a public user to a public user."
+                "Make sure the current tenant {} is not the "
+                "public tenant.".format(self))
+
+        self._check_user_exists(user_obj)
+
+        # The tenant user already linked to a public user
+        if tenant_user.supervisor is not None:
+            raise ExistsError(
+                "The tenant user already linked to public user: {}".format(
+                    tenant_user.supervisor))
+
+        tenant_user.supervisor = user_obj
+        tenant_user.save(update_fields=['supervisor'])
+
+        # Link user to tenant
+        fields = self.__class__.users.field.remote_field.through_fields + (
+            'organization_user',)
+        self.__class__.users.through._default_manager.create(
+            **dict(zip(fields, (self, user_obj, tenant_user.pk))))
+
+        tenant_user_connected.send(sender=self.__class__,
+                                   user=user_obj,
+                                   tenant=self,
+                                   tenant_user=tenant_user)
+
+    @schema_required
+    @transaction.atomic
+    def disconnect_user(self, user_obj, tenant_user):
+        """
+        Disconnect a public user from the tenant user.
+        """
+        if self.schema_name == get_public_schema_name():
+            raise SchemaError(
+                "Make sure the current tenant {} is not "
+                "the public tenant.".format(self))
+
+        disconnected = False
+        # Test that user is already disconnected from the tenant user
+        if self.users.filter(id=user_obj.id).exists() or \
+           get_tenant_user_model().objects.filter(
+               supervisor=user_obj.id).exists():
+            disconnected = True
+
+        if not user_obj.is_active:
+            raise InactiveError(
+                "User specified is not an active user: %s" % user_obj)
+
+        # Don't allow disconnecting an owner from a tenant
+        if user_obj.id == self.owner.id:
+            raise DeleteError(
+                "Cannot disconnect owner from tenant: {}".format(self.owner))
+
+        tenant_user.supervisor = None
+        tenant_user.save(update_fields=['supervisor'])
+
+        self._unlink_from_tenant(user_obj)
+
+        if disconnected:
+            tenant_user_disconnected.send(sender=self.__class__,
+                                          user=user_obj,
+                                          tenant=self,
+                                          tenant_user=tenant_user)
+
     @schema_required
     @transaction.atomic
     def add_user(self, user_obj, is_superuser=False, is_staff=False, **extra_perms):
@@ -58,22 +165,10 @@ class TenantBase(TenantMixin):
         if self.schema_name == get_public_schema_name():
             raise SchemaError(
                 "It's not allowed to add a public user to the public tenant."
-                "Make sure the current tenant {} is not the public tenant.".format(self)
-            )
+                "Make sure the current tenant {} is not the "
+                "public tenant.".format(self))
 
-        # User already is linked here
-        if self.users.filter(id=user_obj.id).exists():
-            raise ExistsError("User already added to tenant: %s" % user_obj)
-
-        # User already linked to a user inside the tenant, due to dirty data
-        if get_tenant_user_model().objects.filter(supervisor_id=user_obj.id).exists():
-            raise ExistsError(
-                'User already linked to a user inside the tenant: {}'.format(
-                    get_tenant_user_model().objects.filter(
-                        supervisor_id=user_obj.id
-                    ).first()
-                )
-            )
+        self._check_user_exists(user_obj)
 
         # Create a user in the tenant with generated username and email
         # And link it to the public user
@@ -82,24 +177,17 @@ class TenantBase(TenantMixin):
             email='{}_{}'.format(user_obj.email, time_string),
             username='{}_{}'.format(user_obj.username, time_string),
             supervisor=user_obj,
-            is_verified=True
-        )
+            is_verified=True)
 
         # Create permissions for this tenant user
         get_permissions_model().objects.create(
             user_id=tenant_user.pk,
             is_staff=is_staff,
             is_superuser=is_superuser,
-            **extra_perms
-        )
+            **extra_perms)
 
         # Link user to tenant
-        fields = self.__class__.users.field.remote_field.through_fields + (
-            'organization_user',
-        )
-        self.__class__.users.through._default_manager.create(
-            **dict(zip(fields, (self, user_obj, tenant_user.pk)))
-        )
+        self._link_to_tenant_user(user_obj, tenant_user)
 
         tenant_user_added.send(sender=self.__class__, user=user_obj, tenant=self)
 
@@ -133,8 +221,7 @@ class TenantBase(TenantMixin):
             raise DeleteError("Cannot remove owner from tenant: %s" % self.owner)
 
         tenant_user = get_tenant_user_model().objects.filter(
-            supervisor=user_obj.id
-        ).first()
+            supervisor=user_obj.id).first()
         if tenant_user:
             # Set supervisor of the tenant user to NULL
             tenant_user.supervisor = None
@@ -156,11 +243,7 @@ class TenantBase(TenantMixin):
                 tenant_user.is_active = False
             tenant_user.save()
 
-        # Unlink from tenant
-        fields = self.__class__.users.field.remote_field.through_fields
-        self.__class__.users.through._default_manager.filter(
-            **dict(zip(fields, (self, user_obj)))
-        ).delete()
+        self._unlink_from_tenant(user_obj)
 
         if deleted:
             tenant_user_removed.send(sender=self.__class__, user=user_obj, tenant=self)
